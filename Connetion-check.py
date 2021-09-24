d2cc29e4609ce7530e5a776e694b9aeae55ba953
An_White <andresblancahernandez@gmail.com>
2021-09-23 21:26:07 -0400

    add Connection-check.py and create a folder for emails_hunter and its samples

---
+++ Connetion-check.py
@@ -0,0 +1,15 @@
+import requests, time
+url='''https://www.google.com/'''
+while True:
+    try:
+        print('Prueba de conexion')
+        start=time.time()
+        test=requests.get(url)
+        end=time.time()
+        ping=end-start
+        print('Conexion exitosa con un ping = '+str(ping))
+        pause=input()
+        break
+    except requests.exceptions.ConnectionError:
+        print('No hay conexión a internet, siguiente prueba en 60s\n')
+        time.sleep(60)
